2cce97808a4ff9ee9eb7f47d8d0cae63d1b70a8b
dbsrlwo1 <dbsrlwo1@naver.com>
2020-08-24 10:46:31 +0900

    [기재] 필요없는 함수 삭제

--- 5.Android/Toilet/app/src/main/java/com/example/project_toilet/Calculate_Method.java
+++
@@ -1,9 +0,0 @@
-package com.example.project_toilet;
-public class Calculate_Method {
-    public boolean getThreshold_Check (float input_len , float last_len , float threshold){
-        if( (input_len/last_len)*100 < threshold){
-            return false;
-        }
-        return true;
-    }
-}
